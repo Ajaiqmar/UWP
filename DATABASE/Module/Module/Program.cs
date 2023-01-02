84e243aac3fd512dd59b954504b74605d9b008d2
AJAY KUMAR T M <54910043+Ajaiqmar@users.noreply.github.com>
2023-01-02 15:28:12 +0530

    Add files via upload

---
+++ DATABASE/Module/Module/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace Program
+{
+    class Lesson
+    {
+        public static void Main(string[] args)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=E:/ZOHO/DATABASE/Module/Module/db/hotel.db;"))
+            {
+                try
+                {
+                    conn.Open();
+
+                    SQLiteCommand comm = conn.CreateCommand();
+                    comm.CommandText = "SELECT * FROM users;";
+
+                    SQLiteDataReader reader = comm.ExecuteReader();
+
+                    while(reader.Read())
+                    {
+                        Console.WriteLine(reader.GetInt32(0)+" "+reader.GetString(1));
+                    }
+                }
+                catch (Exception ex) { }
+            }
+
+        }
+    }
+}
